c60a1add546c7e20f3b2dcaa4adc0f7a24f34f3b
AlbinaKostikova <albucit33@gmail.com>
2025-10-20 19:18:32 +0300

    props hook useState

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,31 @@
-import './Form.scss'
-export const Form = () => {
+import { useState } from 'react';
+import './Form.scss';
+
+export const Form = (props: { createNewToDo: Function }) => {
+  const [text, setText] = useState<string>('');
+
+  const formSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Submit:', text);
+    if (text.trim()) {
+      props.createNewToDo(text);
+      setText('');
+    }
+  };
+
   return (
     <div className="form-wrapper">
-        <form action="#">
-            <label>
-                <input type="text" />
-                <button></button>
-            </label>
-        </form>
+      <form onSubmit={formSubmit}>
+        <label>
+          <input 
+            type="text" 
+            value={text}
+            onChange={(e) => setText(e.target.value)} 
+            placeholder="Введите задачу"
+          />
+        </label>
+        <button type="submit"></button>
+      </form>
     </div>
-  )
-}
+  );
+};
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,38 @@
+import { useState } from 'react'
 import { Form } from '../components/Form/Form'
 import { Header } from '../components/Header/Header'
 import { ToDoList } from '../components/ToDoList/ToDoList'
+import { ToDo } from '../models/todo-item'
 
 export const ToDoListPage = () => {
+  const [todos, setTodos] = useState<ToDo[]>([])
+
+  const createNewToDo = (text: string) => {
+    const newToDo: ToDo = {
+      id: todos.length,
+      text: text,
+      isDone: false,
+    }
+    setTodos([...todos, newToDo])
+  }
+  const updateTodo = (toDoItem: ToDo) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === toDoItem.id) {
+        todo.isDone = !todo.isDone
+      }
+      return todo
+  })
+    setTodos(newTodos)
+  }
+  const deleteTodo = (toDoItem: ToDo) => {
+    const newTodos = todos.filter(todo => todo.id !== toDoItem.id)
+    setTodos(newTodos)
+  }
   return (
     <>
       <Header />
-      <Form />
-      <ToDoList />
+      <Form createNewToDo={createNewToDo} />
+      <ToDoList todos={todos} updateToDo={updateTodo} deleteToDo={deleteTodo} />
     </>
   )
 }
